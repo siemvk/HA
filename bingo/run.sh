4f343beb6bf175f179fb728431bdf72ee22e4119
Siemvk <100954549+siemvk@users.noreply.github.com>
2026-06-22 13:13:38 +0200

    add bingo

---
+++ bingo/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Starting Bun TypeScript Application..."
+
+# Navigate to the app directory
+cd /app
+
+# Run your production script (make sure this script is defined in your package.json)
+# E.g., "start": "bun run src/index.ts"
+bun run prod

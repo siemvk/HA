73e0e1cb3feb63fda7f9214fcb6b44c2795f9cb8
Siemvk <100954549+siemvk@users.noreply.github.com>
2026-07-26 00:59:17 +0200

    aaaaa

--- image_provider/src/server.ts
+++ image_provider/src/server.ts
@@ -18,11 +18,7 @@ type CachedImage = {
 const desktopImageWidth = 1920;
 const desktopImageHeight = 1080;
 const imageCacheControl = "public, max-age=86400, stale-while-revalidate=604800";
-const corsHeaders: HeadersInit = {
-    "access-control-allow-origin": "*",
-    "access-control-allow-methods": "GET, OPTIONS",
-    "access-control-allow-headers": "*",
-};
+const corsHeaders: HeadersInit = {};
 
 const port = 3000;
 const statePath = "/data/current-image.json";
